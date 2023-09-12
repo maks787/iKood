70243abe0222288107854b8b35c167e7a514f2ea
opilane <opilane@HP-CZC2349HT1>
2023-09-12 16:30:30 +0300

    Add project files.

---
+++ IKood.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+public class IKood
+{
+    public string Code { get; set; }
+}
+
+public class HospitalService
+{
+    public static string Haigla(string idCode)
+    {
+        string digits8910 = idCode.Substring(7, 3);
+        int t = int.Parse(digits8910);
+
+        if (t >= 1 && t <= 10)
+        {
+            return "Kuressaare haigla";
+        }
+        else if (t >= 11 && t <= 19)
+        {
+            return "Tartu Ülikooli Naistekliinik";
+        }
+        else if (t >= 21 && t <= 150)
+        {
+            return "Ida-Tallinna keskhaigla, Pelgulinna sünnitusmaja (Tallinn)";
+        }
+        else if (t >= 151 && t <= 160)
+        {
+            return "Keila haigla";
+        }
+        else if (t >= 161 && t <= 220)
+        {
+            return "Rapla haigla, Loksa haigla, Hiiumaa haigla (Kärdla)";
+        }
+        else if (t >= 221 && t <= 270)
+        {
+            return "Ida-Viru keskhaigla (Kohtla-Järve, endine Jõhvi)";
+        }
+        else if (t >= 271 && t <= 370)
+        {
+            return "Maarjamõisa kliinikum (Tartu), Jõgeva haigla";
+        }
+        else if (t >= 371 && t <= 420)
+        {
+            return "Narva haigla";
+        }
+        else if (t >= 421 && t <= 470)
+        {
+            return "Pärnu haigla";
+        }
+        else if (t >= 471 && t <= 490)
+        {
+            return "Haapsalu haigla";
+        }
+        else if (t >= 491 && t <= 520)
+        {
+            return "Järvamaa haigla (Paide)";
+        }
+        else if (t >= 521 && t <= 570)
+        {
+            return "Rakvere haigla," +
+                "" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "" +
+                " Tapa haigla";
+        }
+        else if (t >= 571 && t <= 600)
+        {
+            return "Valga haigla";
+        }
+        else if (t >= 601 && t <= 650)
+        {
+            return "Viljandi haigla";
+        }
+        else if (t >= 651 && t <= 700)
+        {
+            return "Lõuna-Eesti haigla (Võru), Põlva haigla";
+        }
+        else
+        {
+            return "teine ​​haigla";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,674 @@
+﻿using System;
+
+class Program
+{
+    private static List<IKood> iKoodList = new List<IKood>();
+    private static string fileName = "ikood.txt"; 
+
+    static void Main()
+    {
+        LoadIKoodFromFile(); 
+
+        while (true)
+        {
+            Console.WriteLine("Vali tegevus:");
+            Console.WriteLine("1. Add IKood");
+            Console.WriteLine("2. Delete IKood");
+            Console.WriteLine("3. Search IKood");
+            Console.WriteLine("4. Exit");
+            Console.Write("Sisesta valik: ");
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    AddIKood();
+                    break;
+                case "2":
+                    DeleteIKood();
+                    break;
+                case "3":
+                    SearchIKood();
+                    break;
+                case "4":
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("Vigane valik. Proovi uuesti.");
+                    break;
+            }
+        }
+    }
+
+    static void AddIKood()
+    {
+        Console.Write("Sisesta isikukood: ");
+        string idCode = Console.ReadLine();
+
+        if (!IsValidLength(idCode))
+        {
+            Console.WriteLine("Liiga pikk või lühike isikukood");
+        }
+        else if (!idCode.All(char.IsDigit))
+        {
+            Console.WriteLine("Vaja ainult numbrite sisestamine");
+        }
+        else
+        {
+            iKoodList.Add(new IKood { Code = idCode });
+            Console.WriteLine("Isikukood lisatud edukalt.");
+            SaveIKoodToFile(); // Сохраняем IKood в файл после добавления
+        }
+    }
+
+    static void DeleteIKood()
+    {
+        Console.Write("Sisesta isikukood, mida soovid kustutada: ");
+        string idCode = Console.ReadLine();
+
+        IKood iKoodToDelete = iKoodList.FirstOrDefault(i => i.Code == idCode);
+
+        if (iKoodToDelete != null)
+        {
+            iKoodList.Remove(iKoodToDelete);
+            Console.WriteLine("Isikukood kustutatud edukalt.");
+            SaveIKoodToFile(); // Сохраняем IKood в файл после удаления
+        }
+        else
+        {
+            Console.WriteLine("Isikukoodi ei leitud.");
+        }
+    }
+
+    static void SearchIKood()
+    {
+        Console.Write("Sisesta isikukood, mida soovid otsida: ");
+        string idCode = Console.ReadLine();
+
+        IKood iKoodToSearch = iKoodList.FirstOrDefault(i => i.Code == idCode);
+
+        if (iKoodToSearch != null)
+        {
+            Console.WriteLine($"See on {GetGender(idCode)} ta on sündinud {GetBirthDate(idCode)}, tema sünnikoht on {HospitalService.Haigla(idCode)}");
+        }
+        else
+        {
+            Console.WriteLine("Isikukoodi ei leitud.");
+        }
+    }
+
+    static bool IsValidLength(string idCode)
+    {
+        return idCode.Length == 11;
+    }
+
+    static string GetGender(string idCode)
+    {
+        char firstDigit = idCode[0];
+
+        if (new[] { '1', '3', '5' }.Contains(firstDigit))
+        {
+            return "mees";
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        ,
+        else if (new[] { '2', '4', '6' }.Contains(firstDigit))
+        {
+            return "naine";
+        }
+        else
+        {
+            return "viga";
+        }
+    }
+
+    static string GetBirthDate(string idCode)
+    {
+        string year = idCode.Substring(1, 2);
+        string month = idCode.Substring(3, 2);
+        string day = idCode.Substring(5, 2);
+
+        return $"{day}.{month}.{year}";
+    }
+    static void LoadIKoodFromFile()
+    {
+        if (File.Exists(fileName))
+        {
+            string[] lines = File.ReadAllLines(fileName);
+            iKoodList = lines.Select(line => new IKood { Code = line }).ToList();
+        }
+    }
+
+    
+    static void SaveIKoodToFile()
+    {
+        File.WriteAllLines(fileName, iKoodList.Select(i => i.Code));
+    }
+
+}
